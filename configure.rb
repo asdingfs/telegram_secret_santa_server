cd80ded1f5369ad2db66615ebd1d054a6bc05b90
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2017-12-25 20:37:50 +0800

    Setup logging, lock ruby version to use

--- configure.rb
+++ configure.rb
@@ -1,5 +1,16 @@
 # GLOBAL variables
-$TOKEN = ENV["TELEGRAM_BOT_TOKEN"]
+configure do
+  set :telegram_bot_token, ENV["TELEGRAM_BOT_TOKEN"]
+  set :root, File.dirname(__FILE__)
+end
+
+# application-wide configurations
+configure do
+  # logging
+  file = File.new(File.join(settings.root, 'log/application.log'), 'a+')
+  file.sync = true
+  use Rack::CommonLogger, file
+end
 
 # configurations for development and tests
 configure :development, :test do
