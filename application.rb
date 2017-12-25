a4518eeda834f82423af9aac3b4cc43ea0f33ed8
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2017-12-25 13:57:06 +0800

    Define structure for requiring files

--- application.rb
+++ application.rb
@@ -1,3 +1,6 @@
 require 'sinatra'
 
-# TODO: do stuff here
+# require init files
+require_relative 'models/init'
+require_relative 'helpers/init'
+require_relative 'routes/init'
--- config.ru
+++ config.ru
@@ -1,2 +1,3 @@
-require './application'
+root = File.dirname(__FILE__)
+require File.join(root, 'application')
 run Sinatra::Application
