5afc1973a461cda7365eaef93dd05502eaa7bb4d
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2018-01-01 18:04:29 +0800

    Add uniqueness validation to Participant user_id

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,3 +1,5 @@
 class Participant < ActiveRecord::Base
   belongs_to :exchange
+
+  validates_uniqueness_of :user_id
 end
--- db/migrate/20171231045233_create_participants.rb
+++ db/migrate/20171231045233_create_participants.rb
@@ -2,7 +2,11 @@ class CreateParticipants < ActiveRecord::Migration[5.1]
   def change
     create_table    :participants do |t|
       t.references  :exchange
+      t.integer     :user_id
       t.text        :profile
+      t.boolean     :is_set
     end
+
+    add_index :participants, :user_id, unique: true
   end
 end
