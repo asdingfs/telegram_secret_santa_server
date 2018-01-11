9bf5df72f6a3747a9335cd87a21e861fda866828
Anthony Setiawan <anthonysetiawan.ding@gmail.com>
2018-01-12 04:48:47 +0800

    Register participant if /register is called on private chat

--- app/models/updates/private_chat.rb
+++ app/models/updates/private_chat.rb
@@ -10,49 +10,47 @@ module Updates
     end
     def parse
       case
-      when parser.multiple_commands?; parse_multiple_commands
-      when parser.no_command?; parse_no_command
-      else; parse_command
+      when parser.multiple_commands?;   parse_multiple_commands
+      when parser.no_command?;          parse_no_command
+      else;                             parse_command
       end
     end
     def parse_command
-      if active_participant?
-        parse_active
-      else
-        parse_inactive
+      case
+      when set_message?;                parse_set
+      when active_participant?;         parse_active
+      else;                             parse_inactive
       end
     end
 
     ####################### methods
+    def parse_set
+      parser.commands.each do |command|
+        send_message("Sorry! " + Participant.message_set_prompt) # TODO: improve prompt
+      end
+    end
     def parse_active
       parser.commands.each do |command|
         case command
         when '/help'
-          message_set? ?
-            send_message("Hurray!! " + Participant.message_set_prompt) :
-            send_message(Participant.long_help_prompt)
+          send_message(Participant.long_help_prompt)
         when '/edit'
-          message_set? ?
-            send_message("Sorry! " + Participant.message_set_prompt) :
-            edit_message(parser.non_commands)
+          edit_message(parser.non_commands)
         when '/set'
           set_message
         else
-          message_set? ?
-            send_message(Participant.message_set_prompt) :
-            parse_no_command
+          parse_no_command
         end
       end
     end
     def parse_inactive
       parser.commands.each do |command|
         case command
-        when '/start'
-          # TODO:
+        when '/register'
+          register_participant
         else
           send_message(Participant.not_registered_prompt)
         end
-        return
       end
     end
 
@@ -60,7 +58,7 @@ module Updates
     def active_participant?
       !participant.nil?
     end
-    def message_set?
+    def set_message?
       participant && participant.is_set?
     end
     def edit_message(text)
@@ -70,9 +68,16 @@ module Updates
       send_message(edited_message)
     end
     def set_message
-      participant.update(set: true)
+      participant.update!(set: true)
       send_message("Hurray!! " + Participant.message_set_prompt)
       # TODO: shuffle and send preferences is done
+    end
+    def register_participant
+      registration = Registration.
+        where(user_id: message.from.id).
+        first_or_initialize
+      registration.update!(chat_id: message.chat.id)
+      # TODO: improve prompt
     end
     def help_prompt
       Participant.short_help_prompt
